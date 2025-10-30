893a716ba18203303b85a9fdc2aa9a998bc8e865
FlappYes <19mat19@mail.ru>
2025-10-30 12:39:23 +0300

    1

--- Case-Dem-33/App.xaml.cs
+++ Case-Dem-33/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Case_Dem_33.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace Case_Dem_33
     /// </summary>
     public partial class App : Application
     {
+        public static CaseEntities context = new CaseEntities();
     }
 }
